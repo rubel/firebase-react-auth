b5d5cca18d8bcc6bd11cda9ef8f8f1bb4e5e945b
rubel <rubel_xpress@yahoo.com>
2023-04-23 21:04:49 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
           </ul>
         </nav>
       </div>
+              
       <Routes>
         <Route path='/' element={<Dashboard/>}/>
         <Route path='/dashboard' element={<Dashboard/>}/>
